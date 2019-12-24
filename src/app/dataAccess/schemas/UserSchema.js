4cd04340e92a2e487cdbb182a2fb3fc5305e8776
AshutoshGole <ashutoshsgole@gmail.com>
2019-12-24 19:50:47 +0530

    "user schema in signup form addedd "

--- src/app/dataAccess/schemas/UserSchema.js
+++ src/app/dataAccess/schemas/UserSchema.js
@@ -1,22 +1,68 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    name: {
+    image: {
         type: String
+    },
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    username: {
+        type: String
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
     },
     password: {
         type: String,
     },
-    email: {
+    confirmPassword: {
         type: String,
-        required: true,
-        unique: true
     },
-    salt:{
+    type: {
+        type: String,
+    },
+    country: {
+        type: String,
+    },
+    city: {
+        type: String,
+    },
+    introduction: {
+        type: String,
+    },
+    age: {
+        type: String,
+    },
+    interestedIn: {
+        type: String,
+    },
+    educationLevel: {
+        type: String,
+    },
+    majorIndustry: {
+        type: String,
+    },
+    profession: {
+        type: String,
+    },
+    yearsOfExperience: {
+        type: String,
+    },
+    salt: {
         type: String
     },
     token: {
         type: String
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
     },
 }, {
     timestamps: true
