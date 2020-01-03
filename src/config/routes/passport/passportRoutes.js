3c175526f28f9584248a97d7075cbedcc5a23bbe
AshutoshGole <ashutoshsgole@gmail.com>
2020-01-03 19:26:51 +0530

    "google athentication work added html response"

--- src/config/routes/passport/passportRoutes.js
+++ src/config/routes/passport/passportRoutes.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const passport = require('passport');
 const session = require("express-session");
 const utility = require("../../passport/utility");
+const path = require('path');
+const template = require('../../../config/middlewares/templates/googleTemplate');
 
 router.use(session({ secret: process.env.GOOGLE_CLIENT_SECRET, resave: true, saveUninitialized: true }));
 // Initialize Passport and restore authentication state, if any, from the session.
@@ -12,17 +14,27 @@ utility(passport);
 router.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
 
 router.get('/auth/google/callback',
-    passport.authenticate('google', {
-        successRedirect: '/auth/google/success',
-        failureRedirect: '/auth/google/failure'
-    })
+    passport.authenticate('google', { failureRedirect: '/user-type/all-user-types' }), (req, res) => {
+        // console.log(res.req.user);
+        // res.json(res.req.user);
+        res.redirect('/auth/google/success');
+    }
+
+    // passport.authenticate('google', {
+    //     successRedirect: '/auth/google/success',
+    //     failureRedirect: '/auth/google/failure'
+    // })   
 );
 
 router.get('/auth/google/success', (req, res) => {
-    res.send(JSON.stringify('successufully login'))
+    // console.log(req.res.req.user);
+    
+    res.send(template.googleTemplate(req.res.req.user));
+    // res.sendFile(path.join(__dirname + '/data.html'))
 });
+
 router.get('/auth/google/failure', (req, res) => {
-    res.send(JSON.stringify('login failed'))
+    res.sendFile(path.join(__dirname + '/data.html'))
 });
 
 module.exports = router;
