f56a132949305265e4662856b969010b99992971
AshutoshGole <ashutoshsgole@gmail.com>
2020-01-20 17:08:46 +0530

    "added cors"

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const app = require('express')();
+const cors = require('cors');
 
+app.use(cors());
 app.use(require('./src/config/middlewares/base'));
 
 const port = parseInt(process.env.PORT, 10) || 5000;
